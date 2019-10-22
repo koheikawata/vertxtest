49342dc5da9b842e252ac58508e55baae759df7f
koheikawata <kokawata@JPKOKAWATA01.fareast.corp.microsoft.com>
2019-10-22 13:15:54 +0900

    test

---
+++ vetxtest/src/main/java/com/koheikawata/test1.java
@@ -0,0 +1,32 @@
+package com.koheikawata;
+
+import io.vertx.kafka.client.producer.KafkaProducerRecord;
+import io.vertx.kafka.client.producer.KafkaProducer;
+import java.util.HashMap;
+import java.util.Map;
+import io.vertx.core.Vertx;
+
+public class test1
+{
+    public static void main( String[] args )
+    {
+        Vertx vertx = Vertx.vertx();
+
+        Map<String, String> config = new HashMap<>();
+        config.put("bootstrap.servers", "localhost:9092");
+        config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        config.put("acks", "1");
+
+        // use producer for interacting with Apache Kafka
+        KafkaProducer<String, String> producer = KafkaProducer.create(vertx, config);
+
+        for (int i = 0; i < 5; i++) {
+            // only topic and message value are specified, round robin on destination partitions
+            KafkaProducerRecord<String, String> record =
+                KafkaProducerRecord.create("test", "message_" + i);
+
+            producer.write(record);
+        }
+    }
+}
