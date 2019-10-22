6676ba9d36a7dce389c187fd6d4a9dbf41e1724f
Kohei Kawata <kokawata@microsoft.com>
2019-10-23 00:30:29 +0900

    test

--- helloWorld/src/main/java/com/koheikawata/App.java
+++ helloWorld/src/main/java/com/koheikawata/App.java
@@ -1,13 +1,34 @@
 package com.koheikawata;
 
-/**
- * Hello world!
- *
- */
+import io.vertx.kafka.client.producer.KafkaProducerRecord;
+import io.vertx.kafka.client.producer.KafkaProducer;
+import java.util.HashMap;
+import java.util.Map;
+import io.vertx.core.Vertx;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Vertx vertx = Vertx.vertx();
+
+        Map<String, String> config = new HashMap<>();
+        config.put("bootstrap.servers", "localhost:9092");
+        config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        config.put("acks", "1");
+
+        // use producer for interacting with Apache Kafka
+        KafkaProducer<String, String> producer = KafkaProducer.create(vertx, config);
+
+        for (int i = 0; i < 5; i++) {
+            // only topic and message value are specified, round robin on destination partitions
+            KafkaProducerRecord<String, String> record =
+                KafkaProducerRecord.create("test", "message_" + i);
+
+            producer.write(record);
+        }
     }
 }
