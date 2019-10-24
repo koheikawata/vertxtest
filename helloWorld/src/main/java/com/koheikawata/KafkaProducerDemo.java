a62a809b9a549baaf42c53ed60e82e246c5dc38c
Kohei Kawata <kokawata@microsoft.com>
2019-10-24 17:28:41 +0900

    test

--- helloWorld/src/main/java/com/koheikawata/KafkaProducerDemo.java
+++ helloWorld/src/main/java/com/koheikawata/KafkaProducerDemo.java
@@ -19,12 +19,12 @@ public class KafkaProducerDemo
         config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         config.put("acks", "1");
 //        config.put("security.protocol", "SASL_SSL");
-//        config.put("sasl.mechanism", "PLAIN");
-//        config.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"$ConnectionString\" password=\"Endpoint=sb://testkoheieventhubs.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=V7yKG8ubEYifLgJpan6we7DqK9Sse5RcBH5cPQtTuR4=\"");
+        config.put("sasl.mechanism", "PLAIN");
+        config.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"$ConnectionString\" password=\"Endpoint=sb://testkoheieventhubs.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=V7yKG8ubEYifLgJpan6we7DqK9Sse5RcBH5cPQtTuR4=\"");
 
         KafkaProducer<String, String> producer = KafkaProducer.create(vertx, config);
 
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 20; i++) {
             KafkaProducerRecord<String, String> record = KafkaProducerRecord.create("testkoheikafka", "message_" + i);
             producer.write(record);
 
