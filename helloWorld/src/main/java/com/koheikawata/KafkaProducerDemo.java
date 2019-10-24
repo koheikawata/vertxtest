5d2ea5aaeb8407259a208d099f73afe20204f242
Kohei Kawata <kokawata@microsoft.com>
2019-10-24 10:51:34 +0900

    test

--- helloWorld/src/main/java/com/koheikawata/KafkaProducerDemo.java
+++ helloWorld/src/main/java/com/koheikawata/KafkaProducerDemo.java
@@ -10,7 +10,7 @@ public class KafkaProducerDemo
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! \" \\n \\ " );
 
         Vertx vertx = Vertx.vertx();
         Map<String, String> config = new HashMap<>();
@@ -18,7 +18,7 @@ public class KafkaProducerDemo
         config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         config.put("acks", "1");
-        config.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username="$ConnectionString" password="Endpoint=sb://testkoheieventhubs.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=V7yKG8ubEYifLgJpan6we7DqK9Sse5RcBH5cPQtTuR4="");
+        config.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"" + verticleConfig.getUsername() + "\" password=\"" + verticleConfig.getPassword() + "\";");
 
         // use producer for interacting with Apache Kafka
         KafkaProducer<String, String> producer = KafkaProducer.create(vertx, config);
