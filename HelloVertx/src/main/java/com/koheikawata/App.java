0d32578e77154cee992c1c3e1b8ee13008213760
Kohei Kawata <kokawata@microsoft.com>
2019-10-25 10:45:29 +0900

    test

---
+++ HelloVertx/src/main/java/com/koheikawata/App.java
@@ -0,0 +1,40 @@
+package com.koheikawata;
+
+import io.vertx.kafka.client.producer.KafkaProducerRecord;
+import io.vertx.kafka.client.producer.KafkaProducer;
+
+
+import java.util.concurrent.ExecutionException;
+
+import com.koheikawata.IKafkaConstants;
+import com.koheikawata.ProducerCreator;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        runProducer();
+    }
+
+    static void runProducer() {
+        Producer<Long, String> producer = ProducerCreator.createProducer();
+        for (int index = 0; index < IKafkaConstants.MESSAGE_COUNT; index++) {
+//            ProducerRecord<Long, String> record = new ProducerRecord<Long, String>(IKafkaConstants.TOPIC_NAME, "This is record " + index);
+            KafkaProducerRecord<String, String> record = KafkaProducerRecord.create(IKafkaConstants.TOPIC_NAME, "This is record " + index);
+
+            try {
+            RecordMetadata metadata = producer.write(record).get();
+                        System.out.println("Record sent with key " + index + " to partition " + metadata.partition()
+                        + " with offset " + metadata.offset());
+                 } 
+            catch (ExecutionException e) {
+                     System.out.println("Error in sending record");
+                     System.out.println(e);
+                  } 
+             catch (InterruptedException e) {
+                      System.out.println("Error in sending record");
+                      System.out.println(e);
+                  }
+         }
+    }
+}
---
+++ HelloVertx/src/main/java/com/koheikawata/ProducerCreator.java
@@ -0,0 +1,21 @@
+package com.koheikawata;
+
+import io.vertx.core.Vertx;
+import java.util.HashMap;
+import java.util.Map;
+import io.vertx.kafka.client.producer.KafkaProducer;
+
+import com.koheikawata.IKafkaConstants;
+
+public class ProducerCreator {
+    public static Producer<Long, String> createProducer() {
+        Vertx vertx = Vertx.vertx();
+        Map<String, String> config = new HashMap<>();
+        config.put("bootstrap.servers", IKafkaConstants.KAFKA_BROKERS);
+//        config.put(ProducerConfig.CLIENT_ID_CONFIG, IKafkaConstants.CLIENT_ID);
+        config.put("key.serializer", LongSerializer.class.getName());
+        config.put("value.serializer", StringSerializer.class.getName());
+        //props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class.getName());
+        return KafkaProducer.create(vertx, config);
+    }
+}
