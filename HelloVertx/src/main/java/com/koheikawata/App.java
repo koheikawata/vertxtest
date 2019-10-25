a03f9d35d49fe3a25558f0e06cae126c5c8f2f9c
Kohei Kawata <kokawata@microsoft.com>
2019-10-25 10:57:50 +0900

    test

--- HelloVertx/src/main/java/com/koheikawata/App.java
+++ HelloVertx/src/main/java/com/koheikawata/App.java
@@ -22,20 +22,20 @@ public class App
 //            ProducerRecord<Long, String> record = new ProducerRecord<Long, String>(IKafkaConstants.TOPIC_NAME, "This is record " + index);
             KafkaProducerRecord<String, String> record = KafkaProducerRecord.create(IKafkaConstants.TOPIC_NAME, "This is record " + index);
 
-            try {
+//            try {
 //            RecordMetadata metadata = producer.write(record).get();
                 producer.write(record);
 //                        System.out.println("Record sent with key " + index + " to partition " + metadata.partition()
 //                        + " with offset " + metadata.offset());
-                 } 
-            catch (ExecutionException e) {
-                     System.out.println("Error in sending record");
-                     System.out.println(e);
-                  } 
-             catch (InterruptedException e) {
-                      System.out.println("Error in sending record");
-                      System.out.println(e);
-                  }
+//                 } 
+//            catch (ExecutionException e) {
+//                     System.out.println("Error in sending record");
+//                     System.out.println(e);
+//                  } 
+//             catch (InterruptedException e) {
+//                      System.out.println("Error in sending record");
+//                      System.out.println(e);
+//                  }
          }
     }
 }
